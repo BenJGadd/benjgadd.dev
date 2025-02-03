78fa126dcf3cca9ff4ec4d7716352d1bdcde68d3
BenJGadd <benjgadd@gmail.com>
2025-02-02 23:20:24 -0700

    Delete components directory

--- components/navbar.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    const container = document.getElementById('navbar-container');
-    try {
-        container.innerHTML = await (await fetch('components/navbar.html')).text();
-        const navToggle = document.getElementById('nav-toggle');
-    } catch (error) {
-        console.error('Navbar loading failed:', error);
-    }
-}); 
